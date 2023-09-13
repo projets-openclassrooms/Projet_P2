5cd3e81b7af0a26ab0b28177636a86c428b13524
Karine Durand <bk7191014@gmail.com>
2023-09-13 15:33:31 +0200

    test commit

---
+++ scrape_.py
@@ -0,0 +1,34 @@
+#import librairies
+import os
+import csv
+from bs4 import BeautifulSoup as bs
+import requests
+import time
+
+url="https://books.toscrape.com"
+
+#Extraction des données
+response= requests.get(url)
+print(response.headers['content-type']) # 'text/html'
+#print(response.encoding) # 'ISO-8859-1'
+
+if response.ok:
+    links=[]
+    soup= bs(response.text, features="html.parser")
+    products=soup.findAll('<article class="product_pod">')
+    for product in products:
+        a=product.find('a')
+        link=a['href']
+        links.append(url+link)
+    print(links)
+    time.sleep(1)
+
+
+
+liste_products=soup.findAll('All products')
+print(liste_products)
+def get_url(posi):
+    pass
+#Transformation en fichier csv
+
+#Load
