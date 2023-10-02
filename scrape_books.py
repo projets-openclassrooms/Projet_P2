5f7fb861a17ef0ccde2bdafee98212f7aff1d1d0
bk7191 <bk7191014@gmail.com>
2023-10-02 22:33:20 +0200

    modification questionnaire, probleme de liste de categorie à choisir et scraper à corriger.

--- scrape_books.py
+++ scrape_books.py
@@ -1,4 +1,5 @@
 # -*- coding: utf-8 -*-
+import os
 import re
 
 import requests
@@ -27,7 +28,8 @@ sert à récupérer toutes les url d'une seule catégorie
   # utilisation d'une regex pour isoler le nom de la catégorie dans l'url
 
 """
-main_url = "https://books.toscrape.com/"
+home_url = "https://books.toscrape.com/"
+clear = lambda: os.system("cls")
 
 
 # gestion des erreurs de connection sommaire à revoir car bug
@@ -78,7 +80,7 @@ def get_all_url_from_liens(url_liens):
         for h3_tag in h3_tags:
             link_tag = h3_tag.a
             href_value = link_tag["href"].replace("../", "")
-            href_value = f"{main_url}catalogue/{href_value}"
+            href_value = f"{home_url}catalogue/{href_value}"
             categories.append(href_value)
 
     return categories
@@ -100,7 +102,7 @@ def get_all_liens(url):
     hrefs = all_cat.find_all("a", href=True)
     for href in hrefs:  # parcourt les valeurs des hrefs extraits au dessus
         href = href["href"]  # extrait la valeur seule de href
-        url = f"{main_url}{href}"
+        url = f"{home_url}{href}"
         data.append(url)  # ajoute l'url à la liste de données à retourner
     data.pop(0)
 
@@ -115,13 +117,18 @@ def get_all_liens(url):
 def main():
     total_scraped = 0
     n = 0  # compteur
-    print(f"démarrage du scan du site {main_url}")
+    print(f"démarrage du scan du site {home_url}")
 
-    url_home = f"{main_url}index.html"  # url de l'accueil du site
+    url_home = f"{home_url}index.html"  # url de l'accueil du site
     url_category = get_all_liens(url_home)
     question = input("Voulez-vous tester une catégorie ? O/N ")
-    if question == "O":
-        url_categorie = f"{main_url}catalogue/category/books/mystery_3/index.html"
+    if str.lower(question) == "o":
+        i=0
+        for url in url_category:
+            print(i, "+"*2, url)
+            i+=1
+            choix = input("Merci d'indiquer une catégorie de 0 à 49 :")
+            url_categorie = url_category[int(choix)]
         match = re.search(r"\/([^\/]+)_\d+\/", url_categorie)
         name = match.group(1)
         url_liens = get_all_url_from_liens(url_categorie)
@@ -134,11 +141,14 @@ def main():
         print(
             f"catégorie {n} sur {len(url_category)}; dossier : {name} transféré en local avec {len(url_liens)} livres scrapé(s)"
         )
+        clear()
     else:
         print("scrape de toutes les catégories du site")
+        #print("liens des catégories  sauf le home : ",url_category)
         for url in url_category:
             match = re.search(r"\/([^\/]+)_\d+\/", url)
             name = match.group(1)
+            print(name, ":")
             url_liens = get_all_url_from_liens(url)
             scraped_data = scrap_from_url(url_liens, name)
             write_to_csv(scraped_data, name)
