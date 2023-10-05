36e99c660466ef81d133f920ca51a2091b2b63a4
bk7191 <bk7191014@gmail.com>
2023-10-05 16:07:34 +0200

    dir_clean path exist or not + essais pour tous url scrapes

--- save_scrape.py
+++ save_scrape.py
@@ -1,25 +1,23 @@
 import csv
 import os
-
+import shutil
+from pathlib import Path
 
 # ecrit les données dans un fichier csv à partir des données et du nom désiré du fichier
+current_dir = os.path.dirname(__file__)
+folder = os.path.join(current_dir, "/", "site_scraped")
+
 
 def clean_name(name):
     cleaned_name = re.sub(r'[<>;:"/\\|?*]', "-", name)
     return cleaned_name
 
-def dir_clean():
-    try:
-        os.rmdir('site_scraped/')
-        os.rmdir('livres/')
-        os.rmdir('couvertures/')
-    except OSError:
-        pass
-    else:
-        os.mkdir('site_scraped/')
-        os.mkdir('livres/')
-        os.mkdir('couvertures/')
 
+def dir_clean():
+    if Path(os.path.exists(folder)):
+        Path(folder).parent.rmdir()
+    else:
+        Path(folder).parent.mkdir(exist_ok=True, parents=True)
 
 
 def write_to_csv(datas, name):
@@ -38,7 +36,7 @@ def write_to_csv(datas, name):
         "image_url",
     ]
 
-    file_path = os.path.join("site_scraped", name, f"{name}.csv")
+    file_path = os.path.join(folder, name, f"{name}.csv")
     with open(file_path, "w", newline="", encoding="utf-8-sig") as file:
         writer = csv.writer(file, delimiter=";")
         writer.writerow(headers)
@@ -47,4 +45,4 @@ def write_to_csv(datas, name):
         for data in datas:
             writer.writerow(data)
             n += 1  # incrémente le compteur
-            print ('n', n)
+            print('n', n)
--- scrape_books.py
+++ scrape_books.py
@@ -3,7 +3,10 @@ from save_scrape import *
 from scrape_infos import *
 
 """
-sert à récupérer toutes les url d'une seule catégorie
+Ce version alpha sert à récupérer :
+    - soit toutes les infos concernant un livre,
+    - soit les infos concernant tous les livres du site books.toscrape.com
+
     urls = []  # initialisation de la liste servant à stocker nos urls
     categories = []  # initialisation de la liste des datas
     main_url = "https://books.toscrape.com/"
@@ -29,10 +32,12 @@ home_url = "https://books.toscrape.com/"
 # gestion des erreurs de connection sommaire à revoir car bug
 
 
-def get_all_url_from_liens(url_liens):
+def get_all_pages(liens):
+    url_liens = liens
     urls = []
     categories = []
     url_liens_temp = url_liens.split("index")[0]
+    print (url_liens_temp, url_liens)
     while True:
         reponse = requests.get(url_liens)
         page = reponse.content
@@ -54,12 +59,8 @@ def get_all_url_from_liens(url_liens):
             break
 
         for url in urls:
-            try:
-                reponse = requests.get(url)
-            except (
-                    requests.exceptions.RequestException
-            ) as e:  # affiche l'erreur renvoyée
-                print(e)
+            reponse = requests.get(url)
+
             page = reponse.content
             soup = bs(page, "html.parser")
 
@@ -109,7 +110,7 @@ def main():
     total_scraped = 1000  # 1000 à recuperer 20 pages * 50 livres
     compteur = 0  # init compteur
     print(f"démarrage du scan du site {home_url}")
-    dir_clean()  # efface et cree les repertoires de stockage
+
     url_category = get_cat_liens(home_url)
     #print(len(url_category), "catégorires scrapées")
 
@@ -123,6 +124,13 @@ def main():
         choix = input("Merci d'indiquer une catégorie de 0 à 49 :")
         choix_url = url_category[int(choix)]
         print(choix_url, " choisi")
+        name= "Toto"
+        #match = re.search(r"\/([^\/]+)_\d+\/", choix_url)
+        #name = match.group(1)
+
+        #scraped_data = scrap_from_url(choix_url, name)
+        #write_to_csv(scraped_data, name)
+
 
     else:
         print("scrape de toutes les catégories du site")
@@ -132,7 +140,7 @@ def main():
             match = re.search(r"\/([^\/]+)_\d+\/", url)
             name = match.group(1)
             print(name, ":")
-            url_liens = get_all_url_from_liens(url)
+            url_liens = get_all_pages(url)
             for url_book in url_liens:
                 print('book', url_book)
                 scraped_data = scrap_from_url(url_liens, name)
@@ -200,7 +208,10 @@ def main():
 
 if __name__ == "__main__":
     try:
+        #dir_clean()  # efface et cree les repertoires de stockage
         main()
+
+
     except KeyboardInterrupt:
         print("Programme arrêté manuellement.")
 """
