3175727451392e5d1f555574aa6cfdfab97abdcc
bk7191 <bk7191014@gmail.com>
2023-10-10 15:31:44 +0200

    modif mineure

--- test_css.py
+++ test_css.py
@@ -5,22 +5,31 @@ home_url = "https://books.toscrape.com/catalogue/category/books1/"
 
 
 def extract_transform():
-    i= 1
+    i = 1
+    pages = {}
 
     url_pages = f'{home_url}page-{i}.html'
     for url_book in url_pages:
         for i in range(51):
             page = f'page-{i}.html'
             page_scrapped = f'{home_url}{page}'
-            for books in page_scrapped:
-                response = requests.get(books)
+            pages = {'page': page_scrapped}
+        # print(pages)
 
-                # page = reponse.content  # créé une variable avec le contenu de cette réponse
-                soup = bs(response.content, "html.parser")
-                link = soup.css.select('title')
-                print(link)
+    for page in pages.keys():
+        id = page
+        lien = pages[page]
+        print(id, lien)
+        response = requests.get(lien)
+        #     #
+       # page = reponse.content  # créé une variable avec le contenu de cette réponse
+        soup = bs(response.content, "html.parser")
+        link = soup.css.select('title')
+        print(link)
 
-        pass
-    return
+    pass
+
+
+    return link
 
 extract_transform()
